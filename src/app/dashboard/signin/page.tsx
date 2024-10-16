83bd90b2f0d794fd9a14eb779d584b5fec97ca1e
fajarriskihidayat <fajarrh2002@gmail.com>
2024-10-17 06:50:51 +0700

    feat: create ui and integrate sign in page

---
+++ src/app/dashboard/signin/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import { FC } from "react";
+import FormSignIn from "./form";
+
+interface SignInPageProps {}
+
+export const metadata: Metadata = {
+  title: "Dashboard | Sign In",
+};
+
+const SignInPage: FC<SignInPageProps> = () => {
+  return <FormSignIn />;
+};
+
+export default SignInPage;
