9df64306a8a086c124bf77a729255e9d57e936d0
fajarriskihidayat <fajarrh2002@gmail.com>
2024-11-13 14:28:58 +0700

    feat: customer sign up page

---
+++ src/app/(landingpage)/(auth)/sign-up/lib/actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { ActionResult } from "@/app/dashboard/(auth)/signin/form/actions";
+import { userSchema } from "./validation";
+import bcrypt from "bcrypt";
+import prisma from "../../../../../../lib/prisma";
+import { redirect } from "next/navigation";
+
+export async function signUpUser(
+  prevState: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validate = userSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    passport: formData.get("passport"),
+  });
+
+  if (!validate.success) {
+    const errorDesc = validate.error.issues.map((issue) => issue.message);
+
+    return {
+      errorTitle: "Error Validation",
+      errorDesc,
+    };
+  }
+
+  const hashingPassword = bcrypt.hashSync(validate.data.password, 10);
+
+  try {
+    await prisma.user.create({
+      data: {
+        ...validate.data,
+        password: hashingPassword,
+        role: "CUSTOMER",
+      },
+    });
+  } catch (error) {
+    return {
+      errorTitle: "Failed to insert data",
+      errorDesc: ["Terjadi masalah pada koneksi, silahkan coba lagi"],
+    };
+  }
+
+  return redirect("/sign-in");
+}
